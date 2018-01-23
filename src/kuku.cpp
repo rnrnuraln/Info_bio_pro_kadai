e43650ef5a9b1e3067b84b916a73a3cbf8c9bb1e
Kentaro Tara <kid.of.cod428@gmail.com>
2018-01-23 14:47:05 +0900

    added plus function

--- src/kuku.cpp
+++ src/kuku.cpp
@@ -7,12 +7,20 @@ int main(int argc, char *argv[]) {
 		cout << "no enough arguments" << endl;
 		return 0;
 	}
+	bool isPlus = false;
+	if (!strcmp(argv[1], "-p")) {
+		isPlus = true;
+	}
 	int x = stoi(argv[argc - 2]);
 	int y = stoi(argv[argc - 1]);
 	for (int i = 1; i < x + 1; i++) {
 		string s = "";
 		for (int j = 1; j < y + 1; j++) {
-			s += to_string(i * j);
+			int i_j = i * j;
+			if (isPlus) {
+				i_j = i + j;
+			}
+			s += to_string(i_j);
 			s += " ";
 		}
 		cout << s << endl;
