7a37394a41bbf76533a7e2b2f3f81ee633c9a4bf
Kentaro Tara <kid.of.cod428@gmail.com>
2018-01-25 20:50:28 +0900

    included test code

--- src/kuku.cpp
+++ src/kuku.cpp
@@ -1,19 +1,48 @@
 #include <iostream>
 #include <string>
 #include <getopt.h>
+#include <assert.h>
 using namespace std;
+
+string xyToOutput(int x, int y, bool plusFlag) {
+	string s = "";
+	for (int i = 1; i < x + 1; i++) {
+		for (int j = 1; j < y + 1; j++) {
+			int i_j = i * j;
+			if (plusFlag) {
+				i_j = i + j;
+			}
+			s += to_string(i_j);
+			if (i != y) {
+				s += " ";
+			}
+		}
+		if (i != x) {
+			s += "\n";
+		}
+	}
+	return s;
+}
 
 int main(int argc, char *argv[]) {
 	bool plusFlag = false;
 	int c;
-	while ((c = getopt(argc, argv, "p:")) != -1)
+	while ((c = getopt(argc, argv, "pt:")) != -1)
 		switch(c) {
-			case 'p':
+			case 'p': {
 				plusFlag = true;
 				break;
-			default:
+			}
+			case 't': {//for test
+				string s = xyToOutput(1, 1, false);
+				assert(s == "1");
+				s = xyToOutput(1, 1, true);
+				assert(s == "2");
+				break;
+			}
+			default: {
 				abort ();
-			break;
+			}
 		}
 	
 	if (argc < 2) {
@@ -22,17 +51,7 @@ int main(int argc, char *argv[]) {
 	}
 	int x = stoi(argv[argc - 2]);
 	int y = stoi(argv[argc - 1]);
-	for (int i = 1; i < x + 1; i++) {
-		string s = "";
-		for (int j = 1; j < y + 1; j++) {
-			int i_j = i * j; //Š|‚¯ŽZ‚ÌŽÀs
-			if (plusFlag) {
-				i_j = i + j; //‘«‚µŽZ‚ÌŽÀs
-			}
-			s += to_string(i_j);
-			s += " ";
-		}
-		cout << s << endl;
-	}
+	string s = xyToOutput(x, y, plusFlag);
+	cout << s << endl;
 	return 0;
 }
